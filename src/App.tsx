047376612b51210941702f71849640c750ca8416
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-09-17 17:51:42 +0530

    add: styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Node from "./components/dash/Node";
+import Dropdown from "./components/dash/nodeoptions/dropdown";
+import Slider from "./components/dash/nodeoptions/slider";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-background h-screen w-screen">
+      <Node name="EC2" color="#e66d00" />
+      <Slider label="Anisotropic Rotation" min={0} max={100} defaultValue={1.0} step={1} />
+      <Dropdown />
+    </div>
+  );
 };
 
 export default App;

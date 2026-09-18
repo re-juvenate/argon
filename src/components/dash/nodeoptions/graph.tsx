a25e521011f90297e9fa79b927d2501125ff3f30
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-09-19 00:22:29 +0530

    add: graph

--- src/components/dash/nodeoptions/graph.tsx
+++ src/components/dash/nodeoptions/graph.tsx
@@ -1,7 +1,31 @@
-import { AreaChart, Card } from "@tremor/react";
+import { AreaChart } from "@tremor/react";
 
-const Graph = () => {
-  return <div>Graph</div>;
+interface ChartDataItem {
+  date: string;
+  Throughput: number;
+  "Time taken": number;
+}
+
+interface GraphProps {
+  chartdata: ChartDataItem[];
+}
+
+const Graph = ({ chartdata }: GraphProps) => {
+  const numberFormatter = (number: number) => {
+    return Intl.NumberFormat("en-US").format(number);
+  };
+
+  return (
+    <AreaChart
+      className="h-80"
+      data={chartdata}
+      index="date"
+      categories={["Throughput", "Time taken"]}
+      colors={["blue", "emerald"]}
+      valueFormatter={numberFormatter}
+      onValueChange={(v) => console.log(v)}
+    />
+  );
 };
 
 export default Graph;
